bd9b4c96df98708ed7b68d230d4866c778d56132
Phat Hoang <lapsilap@gmail.com>
2020-10-31 16:47:44 +0100

    Add a start game and some function for android to call

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Header from './Components/Header/Header';
 import ScoreBoard from './Components/Score/ScoreBoard';
 import useScore from './hooks/useScore';
 import './App.css';
+
+declare const window: any;
 
 function App() {
   const {
@@ -12,36 +14,28 @@ function App() {
     incScore,
   } = useScore();
 
+  const [gameStarted, setGameStarted] = useState(false);
+
+  useEffect(() => {
+    window.PadelApp = {
+      incScore: (player: number) => {
+        if (gameStarted) {
+          incScore(player);
+        }
+      },
+    }
+  }, [gameStarted])
+
   return (
     <div className="Wrapper">
       <div className="Content">
         <Header />
 
-        <ScoreBoard score1={score1} score2={score2} />
-      </div>
-
-      <div className="Score-buttons">
-        <Button
-            size="small" 
-            variant="contained" 
-            color="secondary"
-            onClick={() => {
-              incScore(1);
-            }}
-        >
-            +1
-        </Button>
-
-        <Button
-            size="small" 
-            variant="contained" 
-            color="secondary"
-            onClick={() => {
-              incScore(2);
-            }}
-        >
-            +1
-        </Button>
+        {
+          gameStarted
+            ? <ScoreBoard score1={score1} score2={score2} />
+            : <Button color="primary" variant="contained" onClick={() => { setGameStarted(true) }}>Start game</Button>
+        }        
       </div>
     </div>
   );
